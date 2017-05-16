7e5df79d3180753a828729a96ee436c5e444edce
atbtm <atbtmmm@gmail.com>
2017-05-16 10:51:35 -0400

    Replace all junit3 patterns with 4. Also added catching FileNotFoundException by ignoring

--- Replace Junit Import/ReplaceJunitImport.groovy
+++ Replace Junit Import/ReplaceJunitImport.groovy
@@ -1,1 +1,24 @@
-Testing
+new File("C:\\view_store\\SF-APPLICATION-SCB-DEV").traverse{ file ->
+	if(file.name.contains(".java")){
+		def changed = false
+		def buff = ""
+		try{
+			file.eachLine{ line ->
+				def changedLine = replaceJunit(line)
+				changed = changed ? changed : !changedLine.equals(line)
+				buff += changedLine +"\n"
+			}
+			if(changed){
+				file.write(buff)
+			}
+		}catch(java.io.FileNotFoundException e){}
+	}
+}
+
+
+def replaceJunit(t){
+    if(t ==~/import (static )?junit.framework.Assert.*/ ){
+        return (t.replaceAll("junit.framework", "org.junit"))
+    }
+    else return t
+}
